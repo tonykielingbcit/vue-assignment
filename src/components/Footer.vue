b5d337703dcbd7e5d6566e6727580cb59208da2d
Tony Kieling <akielingjunior@my.bcit.ca>
2023-03-24 18:36:52 -0700

    calculator being submiited

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,7 @@
 
 <template>
     <footer>
-        <span>SSD - Tony Kieling &copy; - 2023</span>
+        <!-- <span>SSD - Tony Kieling &copy; - 2023</span> -->
+        <span>SSD - 2023</span>
     </footer>
 </template>
